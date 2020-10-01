a69f289017d7063b4ca257868f965f89c96ecd18
Anna Startseva <a.v.starceva@gmail.com>
2020-10-01 16:44:05 +0600

    feat: solution of the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined || array[0] === undefined) return 0;
+    return Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array[0] === undefined) return 0;
+    return Math.max.apply(null, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array[0] === undefined) {
+        return 0;
+    } else {
+        var sum = array.reduce((a, b) => a + b, 0);
+        return sum / (array.length);
+    }
 }
